e83da92febcf0e9129bc1b9dc1dc6c3d30b56a81
Blenda <97736905+BlendaGabrielly@users.noreply.github.com>
2023-07-25 19:39:46 -0300

    fase 1-Inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return <div className="app-container">
+    <div className="register--container">
+      <h1>Loja GameFU</h1>
+      <input type="text" name="name"
+       placeholder='Nome' className="register--input"></input>
+
+      <input type="text" name="cost" placeholder='Preço'
+       className="register--input"></input> 
+
+       <input type="text" name="category" placeholder='Categoria' 
+       className="register--input"></input> 
+    </div>
+     
+    </div>
+}
+
+export default App;
